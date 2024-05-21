a437564739668092e17fa7ba50f7c9b41ea53865
63993 <63993@edu.campusvejle.dk>
2024-05-21 13:31:02 +0200

    animals exercise

--- S0640_animals_exercise.py
+++ S0640_animals_exercise.py
@@ -59,6 +59,8 @@ Send derefter denne Teams-meddelelse til din lærer: <filename> færdig
 Fortsæt derefter med den næste fil."""
 
 
+import random
+
 class Animal:
     def __init__(self, name, sound, height, weight, legs, female):
         self.name = name
@@ -81,19 +83,68 @@ class Dog(Animal):
         self.hunts_sheep = hunts_sheep
 
     def __repr__(self):
-        return f'name: {self.name}, sound: {self.sound}, height: {self.height} cm, weight: {self.weight} kg, legs: {self.legs}, female: {self.female}, tail_length: {self.tail_length} cm, hunts_sheep: {self.hunts_sheep}'
+        return f'name: {self.name}, sound: {self.sound}, height: {self.height} cm, weight: {self.weight} kg, legs: {self.legs}, female: {self.female}, tail_length: {self.tail_length}, hunts_sheep: {self.hunts_sheep}'
 
     def wag_tail(self):
         print(f'Hunden {self.name} logrer med sin {self.tail_length} cm lange hale')
 
-    def mate(self, mother, father):
-        return
+    def mate(self, other):
+        if self.female != other.female:
+            random_number = random.random()
+            if random_number > 0.5:
+                puppy_female = True
+            else:
+                puppy_female = False
+            # puppy_female = random_number > 0.5 # kortere version
+
+            other.female
+            puppy_gender = other.female
+            if puppy_gender:
+                puppy_sound = "Vuuvuvuvuv!"
+            else:
+                puppy_sound = "Vovovovoov!"
+
+            if puppy_female:
+                puppy_height = ((other.height+self.height) / 2) * 0.8
+            else:
+                puppy_height = ((other.height+self.height) / 2) * 1.2
+
+            if puppy_female:
+                puppy_weight = ((other.weight+self.weight) / 2) * 0.9
+            else:
+                puppy_weight = ((other.weight+self.weight) / 2) * 1.1
+
+            if puppy_female:
+                puppy_tail_length = ((other.tail_length+self.tail_length) / 2) * 0.95
+            else:
+                puppy_tail_length = ((other.tail_length+self.tail_length) / 2) * 1.05
+
+            random_number = random.random()
+            puppy_hunts_sheep = random_number > 0.5
+
+            maximum = 4
+            minimum = 3
+            for i in range(5):
+                puppy_legs = random.randint(minimum, maximum)
+            # puppy_sound = "aaa" if puppy_gender else "bbb" # Kortere version af det for oven
+            puppy = Dog('Puppy', sound=puppy_sound, height=puppy_height, weight=puppy_weight, legs=puppy_legs, female=puppy_female, tail_length=puppy_tail_length, hunts_sheep=puppy_hunts_sheep)
+            print(puppy.sound)
+            # print(puppy.legs)
+            return puppy
+
+
+
+
 def main():
-    animal = Animal('Ko', sound='Moooo!', height=140, weight=700, legs=4, female=True)
+    animal = Animal('Ko', sound='Muuuh!', height=140, weight=700, legs=4, female=True)
     animal.make_noise()
 
-    dog = Dog('Snoopy', sound='Vov!', height=12, weight=32, legs=4, female=False, tail_length=12, hunts_sheep=True)
+    dog = Dog('Snoopy', sound='Vov!', height=15, weight=36, legs=4, female=False, tail_length=15, hunts_sheep=True)
     dog.wag_tail()
+    dog2 = Dog('Snoopette', sound='Vuf!', height=12, weight=32, legs=4, female=True, tail_length=12, hunts_sheep=True)
+    new_dog = dog2.mate(dog)
 
+    print(new_dog)
     print(dog)
+    print(dog2)
 main()
