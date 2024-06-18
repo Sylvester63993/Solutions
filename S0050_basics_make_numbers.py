6f41351a14cb7be1f4140fb1e1e687475dbf068f
63993 <63993@edu.campusvejle.dk>
2024-06-18 14:35:18 +0200

    Loop basics: make number

---
+++ S0050_basics_make_numbers.py
@@ -0,0 +1,14 @@
+# Opgave 2: Forandr koden i funktionen sådan at den gør, hvad der står i dens dokumentation.
+
+def make_numbers(start, multiplier, upper_limit):
+    """
+    Documentation Beginning with the start value, this function prints the current value,
+    then multiplies it with multiplier, prints the new current value etc.
+    The function stops when the current value is greater than the upper limit
+                                                                            """
+    current_value = start
+    while current_value < upper_limit:
+      print(current_value)
+      current_value *= multiplier
+
+make_numbers(10, 2, 12345)
