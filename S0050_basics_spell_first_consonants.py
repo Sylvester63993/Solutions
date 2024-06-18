286dcba6e4f8921f69018804303755cf85133a6c
63993 <63993@edu.campusvejle.dk>
2024-06-18 14:36:42 +0200

    Loop basics: First consonants

---
+++ S0050_basics_spell_first_consonants.py
@@ -0,0 +1,16 @@
+# Opgave 5: Forandre koden i funktionen spell_first_consonants() sådan at den gør hvad der står i dens dokumentation.
+
+def spell_first_consonants(text, letter_limit):
+    """
+    Spells/prints the first letter_limit letters of text.
+    Prints only consonants (a, e, i, o, u, y do not get printed). """
+    vowels = ["a", "e", "i", "o", "u", "y"]
+    vowels_included = text[0:letter_limit]
+    vowels_excluded = ""
+    print(vowels_included)
+    for letter in range(len(vowels_included)):
+        if not vowels_included[letter] in vowels:
+            vowels_excluded += vowels_included[letter]
+    print(vowels_excluded)
+    return vowels_excluded
+spell_first_consonants("Hello world", 7)  # should print "Hll w"
