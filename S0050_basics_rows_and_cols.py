cb94b6c22b8b7221c67c1b32428f0b71cd4f56fd
63993 <63993@edu.campusvejle.dk>
2024-06-18 14:46:15 +0200

    For loop basics: Nested for loop

--- S0050_basics_rows_and_cols.py
+++ S0050_basics_rows_and_cols.py
@@ -1,6 +1,6 @@
 """Valgfri on loops/løkker"""
 
-# Opgave 1: Tilføj koden i den næste celle sådan at der udskrives "A1 A2 A3 B1 .... D4 D5".
+# Opgave 1: Tilføj koden i den næste celle sådan at der udskrives "A1 A2 A3 B1 ... D4 D5".
 
 rows = ["A", "B", "C", "D"]
 cols = ["1", "2", "3", "4", "5"]
