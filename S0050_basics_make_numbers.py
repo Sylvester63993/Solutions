41ac36dd7d36e2f80e47e4168768abbcb14ba891
63993 <63993@edu.campusvejle.dk>
2024-06-18 14:42:08 +0200

    For loop basics: Make numbers

--- S0050_basics_make_numbers.py
+++ S0050_basics_make_numbers.py
@@ -5,7 +5,7 @@ def make_numbers(start, multiplier, upper_limit):
     Documentation Beginning with the start value, this function prints the current value,
     then multiplies it with multiplier, prints the new current value etc.
     The function stops when the current value is greater than the upper limit
-                                                                            """
+                                                                             """
     current_value = start
     while current_value < upper_limit:
       print(current_value)
