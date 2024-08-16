78e850be89e85819f71babd2bf2d7793c0ee8a4e
63993 <63993@edu.campusvejle.dk>
2024-08-16 11:24:01 +0200

    GUI exercise 2

--- S2020_gui_exercise2.py
+++ S2020_gui_exercise2.py
@@ -4,7 +4,7 @@ Som altid skal du læse hele opgavebeskrivelsen omhyggeligt, før du begynder at
 
 Kopier denne fil til din egen løsningsmappe. Skriv din løsning ind i kopien.
 
-Bruge det, du har lært i GUI-eksempelfilerne, og byg den GUI, der er afbildet i images/gui_2020.png
+Brug det, du har lært i GUI-eksempelfilerne og byg den GUI, der er afbildet i images/gui_2020.png
 
 Genbrug din kode fra "GUI step 1".
 
