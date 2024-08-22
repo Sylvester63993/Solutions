8fc9130c4e7f88aca98611522e72b3f70de59241
63993 <63993@edu.campusvejle.dk>
2024-08-22 10:59:06 +0200

    DB exercise

---
+++ S2310_db_exercise.py
@@ -0,0 +1,63 @@
+"""
+Som altid skal du læse hele opgavebeskrivelsen omhyggeligt, før du begynder at løse opgaven.
+
+Kopier denne fil til din egen løsningsmappe. Skriv din løsning ind i kopien.
+
+Anvend det, du har lært i dette kapitel om databaser, på en første opgave.
+
+Trin 1:
+Opret en ny SQLite database "S2311_my_second_sql_database.db" i din solutions mappe.
+Denne database skal indeholde 2 tabeller.
+Den første tabel skal hedde "customers" og repræsenteres i Python-koden af en klasse kaldet "Customer".
+Tabellen bruger sin første attribut "id" som primærnøgle.
+De andre attributter i tabellen hedder "name", "address" og "age".
+Definer selv fornuftige datatyper for attributterne.
+
+Trin 2:
+Den anden tabel skal hedde "products" og repræsenteres i Python-koden af en klasse kaldet "Product".
+Denne tabel bruger også sin første attribut "id" som primærnøgle.
+De andre attributter i tabellen hedder "product_number", "price" og "brand".
+
+Trin 3:
+Skriv en funktion create_test_data(), der opretter testdata for begge tabeller.
+
+Trin 4:
+Skriv en metode __repr__() for begge dataklasser, så du kan vise poster til testformål med print().
+
+Til læsning fra databasen kan du genbruge de to funktioner select_all() og get_record() fra S2240_db_class_methods.py.
+
+Trin 5:
+Skriv hovedprogrammet: Det skriver testdata til databasen, læser dataene fra databasen med select_all() og/eller get_record() og udskriver posterne til konsollen med print().
+
+Når dit program er færdigt, skal du skubbe det til dit github-repository.
+Send derefter denne Teams-besked til din lærer: <filename> færdig
+Fortsæt derefter med den næste fil.
+"""
+
+from sqlalchemy.orm import declarative_base, Session  # install sqlalchemy with the command "pip install SQLAlchemy" in a terminal.
+from sqlalchemy import Column, String, Integer  # the library sqlalchemy helps us to work with a database
+from sqlalchemy import create_engine
+
+# The next 2 lines are needed _before_ data classes / sql tables are defined
+Database = 'sqlite:///S2311_my_first_sql_database.db'  # first part: database type, second part: file path
+Base = declarative_base()  # creating the registry and declarative base classes - combined into one step. Base will serve as the base class for the ORM mapped classes we declare.
+
+class Customer(Base):
+    # this class declaration does 2 important things at once:
+    # 1. as usual, it declares a class we can store data in, inside our python program.
+    # 2. it creates a table in a sql database with the specified columns
+    __tablename__ = "customers"
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    address = Column(String)
+    age = Column(Integer)
+
+class Products(Base):
+    # this class declaration does 2 important things at once:
+    # 1. as usual, it declares a class we can store data in, inside our python program.
+    # 2. it creates a table in a sql database with the specified columns
+    __tablename__ = "products"
+    id = Column(Integer, primary_key=True)
+    product_number = Column(Integer)
+    price = Column(Integer)
+    brand = Column(String)
