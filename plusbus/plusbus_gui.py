17ea9689900a611162cd5c6720221c3378648990
63993 <63993@edu.campusvejle.dk>
2024-09-05 13:37:13 +0200

    PlusBus opgave

--- plusbus/plusbus_gui.py
+++ plusbus/plusbus_gui.py
@@ -39,6 +39,44 @@ style.map('Treeview', background=[('selected', treeview_selected)])  # Define co
 # endregion common widgets
 
 # region kunde widgets
+# Define Labelframe which contains all container related GUI objects (data table, labels, buttons, ...)
+labelframe_kunde = tk.LabelFrame(main_window, text="Kunde")  # https://www.tutorialspoint.com/python/tk_labelframe.htm
+labelframe_kunde.grid(row=0, column=0, padx=padx, pady=pady, sticky=tk.N)  # https://www.tutorialspoint.com/python/tk_grid.htm
+
+# Define data table (Treeview) and its scrollbar. Put them in a Frame.
+tree_frame_kunde = tk.Frame(labelframe_kunde)  # https://www.tutorialspoint.com/python/tk_frame.htm
+tree_frame_kunde.grid(row=0, column=0, padx=padx, pady=pady)
+tree_scroll_kunde = tk.Scrollbar(tree_frame_kunde)
+tree_scroll_kunde.grid(row=0, column=1, padx=0, pady=pady, sticky='ns')
+tree_kunde = ttk.Treeview(tree_frame_kunde, yscrollcommand=tree_scroll_kunde.set, selectmode="browse")  # https://docs.python.org/3/library/tkinter.ttk.html#treeview
+tree_kunde.grid(row=0, column=0, padx=0, pady=pady)
+tree_scroll_kunde.config(command=tree_kunde.yview)
+
+# Define the data table's formatting and content
+tree_kunde['columns'] = ("id", "efternavn", "kontakt")  # Define columns
+tree_kunde.column("#0", width=0, stretch=tk.NO)  # Format columns. Suppress the irritating first empty column.
+tree_kunde.column("id", anchor=tk.E, width=40)  # "E" stands for East, meaning Right. Possible anchors are N, NE, E, SE, S, SW, W, NW and CENTER
+tree_kunde.column("efternavn", anchor=tk.E, width=80)
+tree_kunde.column("kontakt", anchor=tk.W, width=200)
+tree_kunde.heading("#0", text="", anchor=tk.W)  # Create column headings
+tree_kunde.heading("id", text="Id", anchor=tk.CENTER)
+tree_kunde.heading("efternavn", text="Efternavn", anchor=tk.CENTER)
+tree_kunde.heading("kontakt", text="Kontakt", anchor=tk.CENTER)
+tree_kunde.tag_configure('oddrow', background=oddrow)  # Create tags for rows in 2 different colors
+tree_kunde.tag_configure('evenrow', background=evenrow)
+
+# Define Frame which contains labels, entries and buttons
+controls_frame_kunde = tk.Frame(labelframe_kunde)
+controls_frame_kunde.grid(row=3, column=0, padx=padx, pady=pady)
+
+# Define data table (Treeview) and its scrollbar. Put them in a Frame.
+tree_frame_kunde = tk.Frame(labelframe_kunde)  # https://www.tutorialspoint.com/python/tk_frame.htm
+tree_frame_kunde.grid(row=0, column=0, padx=padx, pady=pady)
+tree_scroll_kunde = tk.Scrollbar(tree_frame_kunde)
+tree_scroll_kunde.grid(row=0, column=1, padx=0, pady=pady, sticky='ns')
+tree_kunde = ttk.Treeview(tree_frame_kunde, yscrollcommand=tree_scroll_kunde.set, selectmode="browse")  # https://docs.python.org/3/library/tkinter.ttk.html#treeview
+tree_kunde.grid(row=0, column=0, padx=0, pady=pady)
+tree_scroll_kunde.config(command=tree_kunde.yview)
 # endregion kunde widgets
 
 # region main program
