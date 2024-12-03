3a7eef7f97d5cb5d738ee6e47e6cee543dac918a
63993 <63993@edu.campusvejle.dk>
2024-12-03 10:39:23 +0100

    DMI projekt

--- dmi_exercise/dmi_project2.py
+++ dmi_exercise/dmi_project2.py
@@ -5,12 +5,21 @@ KEY = "bf39b989-b47c-4557-abfd-5b3b492aca36"
 
 
 def fetch_data(key=KEY):
-    url = "https://dmigw.govcloud.dk/v2/lightningdata/collections/observation/items?limit=100&api-key=" + key
+    start = input("Indtast start kalenderår: ")
+    end = input("Indtast slut kalenderår: ")
+    date = "datetime=" + start + "-01-01T00:00:00%2B02:00/" + end + "-01-01T00:00:00%2B02:00"
+    url = "https://dmigw.govcloud.dk/v2/lightningdata/collections/observation/items?limit=100&bbox=7,54,16,58&" + date + "&api-key=" + key
     response = requests.get(url)
     data = json.loads(response.text)
     print(f'{data=}')
+    print(data)
+    print("URL: " + url)
+    print("Date: " + date)
+
 
 if __name__ == "__main__":  # Executed when invoked directly
     print(fetch_data(KEY))
-
-# https://dmigw.govcloud.dk/v2/lightningdata/collections/observation/items?datetime=2018-02-12T00:00:00Z/2018-03-18T00:00:00Z&api-key=bf39b989-b47c-4557-abfd-5b3b492aca36
+    # start = input("Indtast start kalenderår: ")
+    # end = input("Indtast slut kalenderår: ")
+    # date = start + "-01-01T00:00:00%2B02:00/" + end + "-01-01T00:00:00%2B02:00"
+    # print(date)
