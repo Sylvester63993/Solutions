83bce4ba69b42cc4279a3a4f44f14f7cad16bc18
63993 <63993@edu.campusvejle.dk>
2024-12-03 13:41:18 +0100

    DMI Projekt

--- dmi_exercise/dmi_project2.py
+++ dmi_exercise/dmi_project2.py
@@ -1,5 +1,7 @@
 import requests
 import json
+import tkinter
+import tkintermapview
 
 KEY = "bf39b989-b47c-4557-abfd-5b3b492aca36"
 
@@ -8,13 +10,25 @@ def fetch_data(key=KEY):
     start = input("Indtast start kalenderår: ")
     end = input("Indtast slut kalenderår: ")
     date = "datetime=" + start + "-01-01T00:00:00%2B02:00/" + end + "-01-01T00:00:00%2B02:00"
-    url = "https://dmigw.govcloud.dk/v2/lightningdata/collections/observation/items?limit=100&bbox=7,54,16,58&" + date + "&api-key=" + key
+    url = "https://dmigw.govcloud.dk/v2/lightningdata/collections/observation/items?limit=10&bbox=7,54,16,58&" + date + "&api-key=" + key
     response = requests.get(url)
     data = json.loads(response.text)
     print(f'{data=}')
     print(data)
     print("URL: " + url)
     print("Date: " + date)
+
+def generate_map():
+    # create tkinter window
+    root_tk = tkinter.Tk()
+    root_tk.geometry(f"{800}x{600}")
+    root_tk.title("map_view_example.py")
+
+    # create map widget
+    map_widget = tkintermapview.TkinterMapView(root_tk, width=800, height=600, corner_radius=0)
+    map_widget.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
+
+    root_tk.mainloop()
 
 
 if __name__ == "__main__":  # Executed when invoked directly
@@ -23,3 +37,5 @@ if __name__ == "__main__":  # Executed when invoked directly
     # end = input("Indtast slut kalenderår: ")
     # date = start + "-01-01T00:00:00%2B02:00/" + end + "-01-01T00:00:00%2B02:00"
     # print(date)
+    generate_map()
+
