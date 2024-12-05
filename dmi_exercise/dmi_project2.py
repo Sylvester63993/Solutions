56235f0fcefe5986c32c32f709afb66700051d65
63993 <63993@edu.campusvejle.dk>
2024-12-05 11:55:10 +0100

    DMI projekt

--- dmi_exercise/dmi_project2.py
+++ dmi_exercise/dmi_project2.py
@@ -3,12 +3,8 @@ import json
 import tkinter
 import tkintermapview
 
-KEY = "bf39b989-b47c-4557-abfd-5b3b492aca36"
-start = "2020"
-end = "2021"
-bbox = "7,54,16,58"
 
-def fetch_data(start, end, bbox, key=KEY):
+def fetch_data(start, end, bbox, key):
     # start = input("Indtast start kalenderår: ")
     # end = input("Indtast slut kalenderår: ")
     date = "datetime=" + start + "-01-01T00:00:00%2B02:00/" + end + "-01-01T00:00:00%2B02:00"
@@ -31,18 +27,25 @@ def generate_map():
     map_widget.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
 
     # set current widget position and zoom
-    # map_widget.set_position(55.613133, 12.356829, marker=True)  # Ishøj Bycenter, Denmark
+    map_widget.set_position(55.613133, 12.356829, marker=True)  # Ishøj Bycenter, Denmark
     # map_widget.set_position(55.9396761, 9.5155848)  # Hele DK, zoom: 7
     # map_widget.set_zoom(7)
 
-    # Gadekæret sø: 55.6174754 12.3482867
+    # Gadekæret sø: (55.6174754), (12.3482867)
     # Jægerbuen sø: 55.6109373 12.3611426
-    map_widget.fit_bounding_box((55.6174754, 12.3482867), (55.6109373, 12.3611426))
+    map_widget.fit_bounding_box((55.6147554, 12.3533761), (55.6114443, 12.3605906))
 
     root_tk.mainloop()
 
 
-if __name__ == "__main__":  # Executed when invoked directly
+def main():
+    KEY = "bf39b989-b47c-4557-abfd-5b3b492aca36"
+    start = "2020"
+    end = "2021"
+    bbox = "7,54,16,58"  # DMIs bbox-koordinater for hele Danmark
     print(fetch_data(start, end, bbox, KEY))
     generate_map()
 
+
+if __name__ == "__main__":  # Executed when invoked directly
+    main()
